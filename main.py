b988eb61f2e3a454c96b1868451e63106305f122
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-09-14 18:27:22 -0700

    tokenizer but tokenize a source code as well

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-import tokenize
 
-def lexeme(intput: str) -> str:
-    d = dict()
+from tokenize import tokenize, untokenize, NUMBER, STRING, NAME, OP
+from io import BytesIO
+
+def decistmt(s):
+    
+    result = []
+    g = tokenize(BytesIO(s.encode('utf-8')).readline)  # tokenize the string
+    for toknum, tokval, _, _, _ in g:
+        if toknum == NUMBER and '.' in tokval:  # replace NUMBER tokens
+            result.extend([
+                (NAME, 'Decimal'),
+                (OP, '('),
+                (STRING, repr(tokval)),
+                (OP, ')')
+            ])
+        else:
+            result.append((toknum, tokval))
+    return untokenize(result).decode('utf-8')
+
+
+#def say_hello():
+#    print("Hello, World!")
+#
+#say_hello()
